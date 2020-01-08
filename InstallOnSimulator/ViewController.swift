cdea40d7befc3908d82551c18d3d0b6acaa77c9a
Ryan Grey <rgrey@scottlogic.co.uk>
2020-01-08 14:28:15 +0000

    Add newly created project

---
+++ InstallOnSimulator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstallOnSimulator
+//
+//  Created by Ryan Grey on 08/01/2020.
+//  Copyright © 2020 ryanggrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
